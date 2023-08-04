167483bcc2216000af9dc8ff4709b36f6f01a497
kosmo <kosmosensei@gmail.com>
2023-08-04 14:53:51 +0700

    Initial commit

---
+++ src/main/java/sky/pro/APILessonGradle/ApiLessonGradleApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.APILessonGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLessonGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLessonGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/APILessonGradle/ApiLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.APILessonGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
